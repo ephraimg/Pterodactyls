d77f570adfb34d0578ac26074b08b5a889b9baf2
Ephraim Glick <ephraim.glick@gmail.com>
2017-11-27 15:38:35 -0800

    Add init func to bug fix asyn issue

--- db/mongo.js
+++ db/mongo.js
@@ -7,18 +7,23 @@ const faker = require('faker');
 const mongoUri = process.env.MONGODB_URI;
 mongoose.Promise = global.Promise;
 
-mongoose.connect(mongoUri, { useMongoClient: true });
+const init = () => {
+  mongoose.connect(mongoUri, { useMongoClient: true });
 
-var db = mongoose.connection;
+  var db = mongoose.connection;
 
-db.on('error', function() {
-  console.log('Mongo connection error:');
-});
+  db.on('error', function() {
+    console.log('Mongo connection error:');
+  });
 
-db.once('open', function() {
-  console.log('Mongo connection successful');
-});
+  db.once('open', function() {
+    console.log('Mongo connection successful');
+  });  
+
+  return Promise.resolve(true);
+};
 
 const Post = mongoose.model('Post', { text: String });
 
 module.exports.Post = Post;
+module.exports.init = init;
